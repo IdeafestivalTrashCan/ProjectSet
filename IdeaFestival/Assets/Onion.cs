8ad2096fae625627d4dc4df06ade66efa9a50e90
hyechxn <a49918072@gmail.com>
2023-12-20 09:30:30 +0900

    Onion!

--- IdeaFestival/Assets/Onion.cs
+++ IdeaFestival/Assets/Onion.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Onion : Monster
 {
+    Animator atkAnimL;
+    Animator atkAnimR;
+
+    Collider2D[] atkArrR;
+    Collider2D[] atkArrL;
+
+    [SerializeField] Vector2 atkSize;
+    private void Awake()
+    {
+        atkAnimR = transform.GetChild(1).GetComponent<Animator>();
+        atkAnimL = transform.GetChild(2).GetComponent<Animator>();
+    }
     private void Start()
     {
         Init(7, 3, 30, 10, 3, false, 5);
@@ -12,11 +24,41 @@ public class Onion : Monster
     protected override void AttackPlay()
     {
         Debug.Log("ATtack GOOGOGOGO");
+        PlayAnim("Attack");
+        Invoke("InflictDamage", 0.3f);
         isAttacking = false;
+    }
+
+    void InflictDamage()
+    {
+        atkArrR = Physics2D.OverlapBoxAll((Vector2)transform.GetChild(1).position, atkSize, 0);
+        atkArrL = Physics2D.OverlapBoxAll((Vector2)transform.GetChild(2).position, atkSize, 0);
         if (IsCheckDistance(attackDistance))
         {
-            player.GetComponent<PlayerHp>().TakeDamage(damage);
+            foreach (Collider2D collider in atkArrR)
+            {
+                if(collider.tag == "Player")
+                    player.GetComponent<PlayerHp>().TakeDamage(damage);
+            }
+            foreach (Collider2D collider in atkArrL)
+            {
+                if (collider.tag == "Player")
+                    player.GetComponent<PlayerHp>().TakeDamage(damage);
+            }
         }
-
     }
+    void PlayAnim(string trigger)
+    {
+        atkAnimL.SetTrigger(trigger);
+        atkAnimR.SetTrigger(trigger);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube((Vector2)transform.GetChild(1).position, atkSize);
+        Gizmos.DrawWireCube((Vector2)transform.GetChild(2).position, atkSize);
+    }
+
+
 }
